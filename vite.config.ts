15d1d472717fd3069cae5e9f025f40ae249610fb
Ribhav <ribhav@coretegra.com>
2025-10-06 21:58:03 +0530

    ffsds

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "https://github.com/Ribhav3/dealfluence-ai-power",
   server: {
     host: "::",
     port: 8080,
